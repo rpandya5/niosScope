ae803edfe89d4ae270460015f43150be31208f57
robertsaabwoo <1robertsolomonsaab@gmail.com>
2025-03-19 20:45:52 -0400

    Update Sinc_graph.c

--- Sinc_graph.c
+++ Sinc_graph.c
@@ -901,14 +901,3 @@ void update(){
 
 
 }
-
-void compress(){
-	for (int i = 0; i<(766/2); i++){
-		//points[i+1][0] = points[i][0];
-		points[766/2 - i+1][1] = points[766/2 - i][1];
-		//points[i+766/2][0] = points[i+766/2+1][0];
-		points[i+766/2][1] = points[i+766/2+1][1];
-	
-	}
-
-}
